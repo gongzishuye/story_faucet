5e576e5564ae6a13968bc573621abb597f0aa255
gongzishuye <clbupt@126.com>
2025-01-07 21:14:42 +0800

    fix: None proxy

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from modules.faucet import Faucet
 from modules.tools import load_wallets, load_proxies, logger, request_proxy_format, sleeping
 from settings import SHUFFLE_WALLETS, INFINITY_MODE
 
-def process_address(address: str, proxy: dict):
+def process_address(address: str, proxy: dict | None):
     try:
-        logger.info(f'Account {address} started! {proxy["http"]}')
+        logger.info(f'Account {address} started! {proxy["http"] if proxy else "None"}')
         faucet = Faucet(address, proxy)
         faucet.claim_tokens()  # 保存返回值
     except AttributeError as e:
@@ -17,7 +17,7 @@ def process_address(address: str, proxy: dict):
     except Exception as e:
         logger.error(f'Account {address} error!\n{str(e)}')
 
-async def process_address_group(addresses: List[str], accounts_proxy: List[dict]):
+async def process_address_group(addresses: List[str], accounts_proxy: List[dict] | None):
     tasks = [
         asyncio.to_thread(
             process_address, 
